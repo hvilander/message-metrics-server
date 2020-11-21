db44e6b6cb7c6c43c96d10506daef41f8cc30b56
Harold <harold.vilander@gmail.com>
2020-11-21 14:13:41 -0600

    emitting_messages: csv file read and each line emitted as json

--- app.js
+++ app.js
@@ -5,13 +5,61 @@ const socketIo = require("socket.io");
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 
+const fs = require('fs');
+const readline = require('readline');
+
 const app = express();
 app.use(index);
 
 const server = http.createServer(app);
-const io = socketIo(server, { cors: { origin: '*' } }); // 
-// CORS policy 'Access-Control-Allow-Origin' fix https://stackoverflow.com/questions/24058157/socket-io-node-js-cross-origin-request-blocked
 
+// CORS policy 'Access-Control-Allow-Origin' fix https://stackoverflow.com/questions/24058157/socket-io-node-js-cross-origin-request-blocked
+// note this is not really a fix, in a production environment this would not be good. Really should require https, and better cross site security
+const io = socketIo(server, { cors: { origin: '*' } });
+
+
+/**
+ * parses a line that was pulled out of the csv file 
+ */
+const parseLine = (line) => {
+  const messageObj = {};
+  const headers = ['message_id','dlc','payload','puc_id','ts','gps_id','latitude','longitude','groundspeed','truecourse'];
+  const messageString = line.split(',');
+
+  for(let i = 0; i < headers.length; i++) {
+    const value = messageString[i];
+    if(value && value != ''){
+      messageObj[headers[i]] = messageString[i];
+    }
+  }
+
+  return messageObj;
+}
+
+// taken from example at https://nodejs.org/api/readline.html#readline_example_read_file_stream_line_by_line
+async function processLineByLine(socket) {
+  try {
+    const fileStream = fs.createReadStream('mock/gps_can_data_small.csv');
+
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+    // Note: we use the crlfDelay option to recognize all instances of CR LF
+    // ('\r\n') in input.txt as a single line break.
+
+    for await (const line of rl) {
+      // Each line in input.txt will be successively available here as `line`.
+
+      // Electing to parse into json here. It could be done on the front end if desired.
+      // The logic is the same in either place. Since there is likely a lot of expected messages,
+      // I could also see where we would get a dumb of many lines to the front end too. 
+      socket.emit("FromAPI", parseLine(line));
+    }
+  } catch (error) {
+    console.log('error: ', error);
+  }
+}
 
 let interval;
 
@@ -20,7 +68,7 @@ io.on("connection", (socket) => {
   if (interval) {
     clearInterval(interval);
   }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  interval = setInterval(() => processLineByLine(socket), 1000);
   socket.on("disconnect", () => {
     console.log("Client disconnected");
     clearInterval(interval);
@@ -31,6 +79,7 @@ io.on("connection", (socket) => {
 
 const getApiAndEmit = socket => {
   const response = new Date();
+
   // Emitting a new message, will be consumed by client
   socket.emit("FromAPI", response);
 };
